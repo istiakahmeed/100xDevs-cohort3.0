a543d8cf32cfef628bc9d0cdd1d0f6cac4c7f0fb
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-08-08 21:53:45 +0600

    some learning variable and data type

---
+++ week-1/basics-js/variable/assignment.js
@@ -0,0 +1,10 @@
+var favColor = "Yellow";
+let favFood = "Pizza";
+const height = 5.5;
+
+console.log("Favorite color: " + favColor);
+
+console.log("Favorite food: " + favFood);
+favFood = "Burger"; // assign new food
+console.log("Favorite food: " + favFood);
+console.log("Height: " + height + " meters");
---
+++ week-1/basics-js/variable/variable.js
@@ -0,0 +1,6 @@
+var isStudent = true; // Older way to declare variables, function-scoped
+let name = "Minal"; // Variable that can be reassigned
+const age = 30; // Constant variable that cannot reassigned
+console.log(isStudent);
+console.log(name);
+console.log(age);
