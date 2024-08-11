8d4f3dd978c907ecc4a6a754c641036db16c306e
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-08-11 19:31:40 +0600

    learn about functions

---
+++ week-1/basics-js/function/assignment.js
@@ -0,0 +1,17 @@
+// Assignments #1
+// Write a function sum that finds the sum of two numbers. Side quest - Try passing in a string instead of a number and see what happens?
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(5, 3)); // Output: 8
+
+// Assignments #2
+// Write a function called canVote that returns true or false if the age of a user is > 18
+
+function canVote(age) {
+  return age >= 18;
+}
+
+console.log(canVote(21)); // Output: true
