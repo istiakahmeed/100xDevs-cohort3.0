2bd4a624406324670e0db90ee965da9d6dc9d79f
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-08-11 19:32:01 +0600

    learn about the if else statement

---
+++ week-1/basics-js/if_else/if_else.js
@@ -0,0 +1,13 @@
+if (age >= 18) {
+  console.log("You can vote!");
+} else {
+  console.log("You are not old enough to vote.");
+}
+
+const name = "John Doe";
+
+if (name === "John Doe") {
+  console.log("Hello, John Doe!");
+} else {
+  console.log("Hello, stranger!");
+}
