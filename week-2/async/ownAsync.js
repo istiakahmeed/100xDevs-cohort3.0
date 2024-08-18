fc3320eba79bac40d0110ebbc9dff3aad4451d21
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-08-18 09:39:16 +0600

    own async

---
+++ week-2/async/ownAsync.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+function cleanFile(filePath, cb) {
+  fs.readFile(filePath, "utf-8", function (err, data) {
+    data = data.trim();
+    fs.writeFile(filePath, data, function () {
+      cb();
+    });
+  });
+}
+
+function onDone() {
+  console.log("file has been cleaned");
+}
+cleanFile("a.txt", onDone);
+
+// Promisified approach
+
+const fs = require("fs");
+function cleanFile(filePath, cb) {
+  return new Promise(function (resolve) {
+    fs.readFile(filePath, "utf-8", function (err, data) {
+      data = data.trim();
+      fs.writeFile(filePath, data, function () {
+        resolve();
+      });
+    });
+  });
+}
+
+async function main() {
+  await cleanFile("a.txt");
+  console.log("Done cleaning file");
+}
+
+main();
